36dd06dbce6fee24fc4f75263e9f6a3306a70992
arel <marcellinus.arel@mitrais.com>
2022-03-24 11:44:53 +0700

    implement slice

--- src/index.js
+++ src/index.js
@@ -1,72 +1,60 @@
 import store from "./store/store";
-import * as actions from './store/product/actionTypes';
+import { productAdded, productMarkAsDiscount } from "./store/product";
 
 
 store.subscribe(() => {
     console.log('Store changed!', store.getState());
 });
 
+store.dispatch(productAdded({
+    name: "Product 1",
+    price: 1500,
+    hasDiscount: false,
+}))
 
-store.dispatch({
-    type: actions.PRODUCT_ADDED,
-    payload: {
-        name: "Product 1",
-        price: 1500,
-        hasDiscount: false,
-    }
-});
+store.dispatch(productAdded({
+    name: "Product 2",
+    price: 500,
+    hasDiscount: false,
+}))
 
+store.dispatch(productAdded({
+    name: "Product AA",
+    price: 100,
+    hasDiscount: false,
+}))
 
+store.dispatch(productAdded({
+    name: "Product B",
+    price: 200,
+    hasDiscount: false,
+}))
 
-store.dispatch({
-    type: actions.PRODUCT_ADDED,
-    payload: {
-        name: "Product 2",
-        price: 500,
-        hasDiscount: false,
-    }
-});
+store.dispatch(productMarkAsDiscount({
+    id: 2,
+    hasDiscount: true,
+}))
 
-store.dispatch({
-    type: actions.PRODUCT_ADDED,
-    payload: {
-        name: "Product aaaa",
-        price: 500,
-        hasDiscount: false,
-    }
-});
+store.dispatch(productAdded({
+    name: "Product C",
+    price: 1200,
+    hasDiscount: true,
+}))
 
-store.dispatch({
-    type: actions.PRODUCT_ADDED,
-    payload: {
-        name: "Product bbb",
-        price: 500,
-        hasDiscount: false,
-    }
-});
+// store.dispatch({
+//     type: actions.PRODUCT_REMOVED,
+//     payload: {
+//         id: 3,
+//     }
+// })
 
-store.dispatch({
-    type: actions.MARK_AS_DISCOUNTED,
-    payload: {
-        id: 2,
-        hasDiscount: true
-    }
-});
-
-store.dispatch({
-    type: actions.PRODUCT_REMOVED,
-    payload: {
-        id: 3,
-    }
-})
-
-store.dispatch({
-    type: actions.PRODUCT_ADDED,
-    payload: {
-        name: "Product ccc",
-        price: 500,
-        hasDiscount: false,
-    }
-});
+// store.dispatch({
+//     type: actions.PRODUCT_ADDED,
+//     payload: {
+//         name: "Product ccc",
+//         price: 500,
+//         hasDiscount: false,
+//     }
+// });
 
 console.log(store.getState())
--- src/store/store.js
+++ src/store/store.js
@@ -1,11 +1,22 @@
-import { createStore } from "redux";
-import reducer from './product/reducer';
-import { devToolsEnhancer } from "@redux-devtools/extension";
+// import { createStore } from "redux";
+// import reducer from './product/reducer';
+// import { devToolsEnhancer } from "@redux-devtools/extension";
 
-const store = createStore(
-    reducer,
-    // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-    devToolsEnhancer({ trace: true })
-);
+
+
+// const store = createStore(
+//     reducer,
+//     // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+//     devToolsEnhancer({ trace: true })
+// );
+
+// export default store;
+
+import { configureStore } from "@reduxjs/toolkit";
+import reducer from './product';
+
+const store = configureStore({
+    reducer
+});
 
 export default store;
